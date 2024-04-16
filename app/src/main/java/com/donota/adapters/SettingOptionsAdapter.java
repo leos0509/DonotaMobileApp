1ec93e63058851060e0858cbb83cd2c8b6455b24
tmphuong0103 <phuongtm0103@gmail.com>
2024-04-16 18:29:20 +0700

    adapter setting options

---
+++ app/src/main/java/com/donota/adapters/SettingOptionsAdapter.java
@@ -0,0 +1,73 @@
+package com.donota.adapters;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.donota.donotamobileapp.R;
+import com.donota.models.SettingOptions;
+
+import java.util.List;
+
+public class SettingOptionsAdapter extends BaseAdapter {
+    Activity activity;
+    int setting_items_layout;
+    List<SettingOptions> settingOptionsList;
+
+    public SettingOptionsAdapter(Activity activity, int setting_items_layout, List<SettingOptions> settingOptionsList) {
+        this.activity = activity;
+        this.setting_items_layout = setting_items_layout;
+        this.settingOptionsList = settingOptionsList;
+    }
+
+
+    @Override
+    public int getCount() {
+        return settingOptionsList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return settingOptionsList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        ViewHolder holder;
+        if (view == null){
+            holder = new ViewHolder();
+            LayoutInflater layoutInflater = (LayoutInflater) activity.getSystemService(LAYOUT_INFLATER_SERVICE);
+            view = layoutInflater.inflate(setting_items_layout, null);
+            holder.imvSettingIcon = view.findViewById(R.id.imvSettingIcon);
+            holder.txtSettingName = view.findViewById(R.id.txtSettingName);
+            holder.imvNext = view.findViewById(R.id.imvNext);
+            view.setTag(holder);
+        }else{
+            holder = (ViewHolder) view.getTag();
+        }
+        SettingOptions settingOptions = settingOptionsList.get(position);
+        holder.imvSettingIcon.setImageResource(settingOptions.getSettingIcon());
+        holder.txtSettingName.setText(settingOptions.getSettingName().toString());
+        holder.imvNext.setImageResource(R.drawable.baseline_navigate_next);
+        return view;
+    }
+    public static class ViewHolder{
+        ImageView imvSettingIcon, imvNext;
+        TextView txtSettingName;
+    }
+}
+
--- app/src/main/java/com/donota/donotamobileapp/AccountActivity.java
+++ app/src/main/java/com/donota/donotamobileapp/AccountActivity.java
@@ -1,5 +1,6 @@
 package com.donota.donotamobileapp;
 
+import android.app.Dialog;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +9,41 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.donota.adapters.SettingOptionsAdapter;
+import com.donota.donotamobileapp.databinding.ActivityAccountBinding;
+import com.donota.models.SettingOptions;
+
+import java.util.ArrayList;
+
 public class AccountActivity extends AppCompatActivity {
+
+    ActivityAccountBinding binding;
+    Dialog dialog;
+
+    SettingOptionsAdapter settingOptionsAdapter;
+    ArrayList<SettingOptions> settingOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_account);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityAccountBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        loadData();
+    }
+
+    private void initData() {
+        settingOptions = new ArrayList<>();
+        settingOptions.add(new SettingOptions("personal_info", R.drawable.baseline_person, "Tai khoan"));
+        settingOptions.add(new SettingOptions("setting", R.drawable.baseline_settings, "Cai dat chung"));
+        settingOptions.add(new SettingOptions("payment_setting", R.drawable.baseline_credit_card, "Cai dat thanh toan"));
+        settingOptions.add(new SettingOptions("address_setting", R.drawable.baseline_location, "Cai dat dia chi"));
+        settingOptions.add(new SettingOptions("policy_setting", R.drawable.baseline_policy, "Chinh sach"));
+    }
+
+    private void loadData() {
+        initData();
+        settingOptionsAdapter = new SettingOptionsAdapter(AccountActivity.this, R.layout.setting_items_layout, settingOptions);
+        binding.lvSettingOptions.setAdapter(settingOptionsAdapter);
     }
 }
