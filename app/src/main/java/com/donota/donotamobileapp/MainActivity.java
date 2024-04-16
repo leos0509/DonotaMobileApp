60707339f9b5bb87307da5728adde4e42ee91444
Nhu Y <nnnhuy111@gmail.com>
2024-04-16 17:06:22 +0700

    Login, Register, Forgot Pw, Reset Pw, Confirm OTP

--- app/src/main/java/com/donota/donotamobileapp/MainActivity.java
+++ app/src/main/java/com/donota/donotamobileapp/MainActivity.java
@@ -1,24 +1,61 @@
 package com.donota.donotamobileapp;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.donota.donotamobileapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        addEvents();
+    }
+
+    View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            FragmentManager manager = getSupportFragmentManager();
+            FragmentTransaction transaction = manager.beginTransaction();
+
+            Fragment fragment = null;
+            String tag = null;
+
+            if (view.equals(binding.btnSignUp)) {
+                fragment = new RegisterFragment();
+                tag = "Register";
+
+            } else if (view.equals(binding.btnLogIn)) {
+                fragment = new LogInFragment();
+                tag = "Log In";
+            }
+
+            if (fragment != null && tag != null) {
+                transaction.replace(R.id.containerLogInLayout, fragment, tag);
+                transaction.addToBackStack(tag);
+                transaction.commit();
+            }
+        }
+    };
+
+    private void addEvents() {
+        binding.btnSignUp.setOnClickListener(clickListener);
+        binding.btnLogIn.setOnClickListener(clickListener);
     }
 }
