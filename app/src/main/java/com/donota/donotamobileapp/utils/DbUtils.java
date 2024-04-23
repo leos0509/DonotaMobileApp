5cad95d12bfb8be907d69d4b0520ca95ad8844ab
tmphuong0103 <phuongtm0103@gmail.com>
2024-04-23 16:04:48 +0700

    ordermangement (not done)

--- app/src/main/java/com/donota/donotamobileapp/utils/DbUtils.java
+++ app/src/main/java/com/donota/donotamobileapp/utils/DbUtils.java
@@ -1,42 +1,37 @@
-package com.donota.donotamobileapp.utils;
+package com.donota.donotamobileapp.Utils;
 
 public class DbUtils {
     public static final String DB_NAME = "donotadb.db";
-    public static final int DB_VERSION = 1;
 
     public static final String DB_FOLDER = "/databases/";
-    public static final String TBL_CUSTOMER_PROFILE = "tbcustomerprofile";
-    public static final String COL_CUSTOMER_ID = "customerid";
-    public static final String COL_CUSTOMER_NAME = "customername";
-    public static final String COL_CUSTOMER_PHONENUMB = "phonenumb";
-    public static final String COL_CUSTOMER_PURCHASE_HISTORY = "purchasehistory";
-    public static final String COL_CUSTOMER_ACCOUNT = "customeraccount";
-    public static final String COL_CUSTOMER_ACCOUNT_PASSWORD = "customeraccountpassword";
-    public static final String COL_CUSTOMER_DOB = "customerdob";
-    public static final String COL_CUSTOMER_EMAIL = "customeremail";
+    public static final String TBL_CUSTOMER_PROFILE = "customerprofile";
+    public static final String COL_CUSTOMER_ID = "CustomerId";
+    public static final String COL_CUSTOMER_FIRSTNAME = "FirstName";
+    public static final String COL_CUSTOMER_LASTNAME = "LastName";
+    public static final String COL_CUSTOMER_PHONENUMB = "PhoneNumb";
+    public static final String COL_CUSTOMER_ADDRESS = "Address";
+    public static final String COL_CUSTOMER_PURCHASE_HISTORY = "PurchaseHistory";
+    public static final String COL_CUSTOMER_ACCOUNT = "CustomerAccount";
+    public static final String COL_CUSTOMER_ACCOUNT_PASSWORD = "CustomerAccountPassword";
+    public static final String COL_CUSTOMER_CART = "CustomerCart";
+    public static final String COL_CUSTOMER_WISHLIST = "wishlist";
+    public static final String COL_CUSTOMER_DOB = "CustomerDOB";
 
-    public static final String TBL_PRODUCT = "tbproduct";
-    public static final String COL_PRODUCT_ID = "productid";
-    public static final String COL_PRODUCT_NAME = "productname";
-    public static final String COL_PRODUCT_PRICE = "productprice";
-    public static final String COL_PRODUCT_QUANTITY = "productquantity";
-    public static final String COL_PRODUCT_CATEGORY = "productcategory";
-    public static final String COL_PRODUCT_DESCRIPTION = "productdescription";
-    public static final String COL_PRODUCT_IMG = "productimg";
-    public static final String COL_PRODUCT_RATING = "productrating";
-    public static final String TBL_ORDER = "tborder";
-    public static final String TBL_ORDER_DETAIL = "tborderdetail";
-    public static final String COL_ORDER_ID = "orderid";
-    public static final String COL_ORDER_VALUE = "ordervalue";
-    public static final String COL_ORDER_DETAIL_ID = "customerdetailid";
-    public static final String COL_ORDER_DATE = "orderdate";
-    public static final String COL_DELIVERY_DATE = "deliverydate";
-    public static final String COL_ORDER_QUANTITY = "orderquantity";
-
-    public static final String TBL_CUSTOMER_CART = "tbcustomercart";
-    public static final String COL_CUSTOMER_CART_QUANTITY = "quantity";
-    public static final String TBL_CUSTOMER_WISHLIST = "tbcustomerwishlist";
-    public static final String TBL_CUSTOMER_ADDRESS = "tbcustomeraddress";
-    public static final String COL_CUSTOMER_ADDRESS = "address";
+    public static final String TBL_PRODUCT = "product";
+    public static final String COL_PRODUCT_ID = "ProductId";
+    public static final String COL_PRODUCT_NAME = "ProductName";
+    public static final String COL_PRODUCT_PRICE = "ProductPrice";
+    public static final String COL_PRODUCT_QUANTITY = "ProductQuantity";
+    public static final String COL_PRODUCT_CATEGORY = "ProductCategory";
+    public static final String COL_PRODUCT_DESCRIPTION = "ProductDescribtion";
+    public static final String COL_PRODUCT_IMG = "ProductImg";
+    public static final String COL_PRODUCT_RATING = "ProductRating";
+    public static final String TBL_ORDER = "order";
+    public static final String TBL_ORDER_DETAIL = "orderdetail";
+    public static final String COL_ORDER_ID = "OrderID";
+    public static final String COL_ORDER_VALUE = "OrderValue";
+    public static final String COL_ORDER_DETAIL_ID = "CustomerDetailId";
+    public static final String COL_ORDER_DATE = "OrderDate";
+    public static final String COL_DELIVERY_DATE = "DeliveryDate";
 
 }
